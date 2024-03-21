0ebbc079a340554cbaf90454343155745d9d3302
Pavel Saikou <60567324+pavelsaikov@users.noreply.github.com>
2024-03-21 15:27:37 +0300

    test c 1

--- apps/cp/src/app/app.tsx
+++ apps/cp/src/app/app.tsx
@@ -1,9 +1,4 @@
-// eslint-disable-next-line @typescript-eslint/no-unused-vars
 import { lib1 } from '@libs/lib-1';
-import styles from './app.module.css';
-
-import NxWelcome from './nx-welcome';
-
 export function App() {
   return (
     <div>
